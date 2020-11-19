7121cdd32bf12fcfe0845d3aeab5b85ec7c03e90
Denilsen Axel <55840688+DenilsenAxel@users.noreply.github.com>
2020-11-19 21:43:46 +0800

    refactor encounter_enemy

--- encounter_enemy.pl
+++ encounter_enemy.pl
@@ -1,7 +1,6 @@
-
 /* File : encounter_simulation.pl */
 
-:- include('enemy.pl').
+%:- include('enemy.pl').
 
 :- dynamic(type_enemy/1).
 :- dynamic(hp_enemy/1).
@@ -11,32 +10,54 @@
 
 /* Encounter enemy randomizer */
 
-encounter_enemy(MaxId, EnemyType) :-
+encounter_enemy(MaxWeight, EnemyType) :-
     random(1, 151, EncounterRate),
-    EncounterRate =< 150,
-    random(0, MaxId, EncounterType),
+    EncounterRate =< 100,
+    random(0, MaxWeight, EncounterWeight),
+
+    enemy_gacha(EncounterWeight),
+    type_enemy(EncounterType),
+
+    /* Print type musuh yang di-encounter */
     EnemyType is EncounterType,
     write('You encounter a '),
     enemy_type(EncounterType, EncounterName),
     write(EncounterName),
     write('!\n').
 
-encounter :- 
-    encounter_enemy(3, X), !,
+/* Rules untuk nge-gacha kemunculan enemy berdasarkan range weight-nya */
+/* Nanti weight-nya bisa disesuaiin sama area map, 
+   biar kemunculan tipe monster tertentu bisa lebih tinggi di tempat tertentu */
+enemy_gacha(Rate) :-
+    Rate =< 50,
     retract(type_enemy(_)),
-    assertz(type_enemy(X)),
+    asserta(type_enemy(0)).
+
+enemy_gacha(Rate) :- 
+    Rate =< 80,
+    Rate > 50,
+    retract(type_enemy(_)),
+    asserta(type_enemy(1)).
+
+enemy_gacha(Rate) :- 
+    Rate =< 100,
+    Rate > 80,
+    retract(type_enemy(_)),
+    asserta(type_enemy(2)).
+
+/* Rules yang bakal dipakai di battle.pl */
+encounter :- 
+    encounter_enemy(100, X), !,
 
     random(1, 6, EnemyLevel),
     retract(lvl_enemy(_)),
     assertz(lvl_enemy(EnemyLevel)),
     
-
     enemy_health(X, Hp),
     ScaleHp is Hp + 10*EnemyLevel,
     retract(hp_enemy(_)),
     assertz(hp_enemy(ScaleHp)),
     
-
     enemy_attack(X, Att),
     ScaleAtt is Att + 5*EnemyLevel,
     retract(att_enemy(_)),
