486771af03449bdc94d4da8ae11a888ba3151e5c
Jesson Yo <57820472+Aphostrophy@users.noreply.github.com>
2020-11-20 00:34:32 +0700

    Player sudah dibuat, belum memperhitungkan bonus items

---
+++ player.pl
@@ -0,0 +1,66 @@
+/* FILE player.pl */
+/* Storing player datas */
+
+/* Dependency Files : items.pl */
+
+:- dynamic(player_class/1).
+:- dynamic(player_level/1).
+:- dynamic(equipped_weapon/1).
+:- dynamic(player_health/1).
+:- dynamic(player_attack/1).
+:- dynamic(player_defense/1).
+
+:- dynamic(player_max_health/1).
+:- dynamic(player_max_attack/1).
+:- dynamic(player_max_defense/1).
+
+:- dynamic(current_gold/1).
+:- dynamic(current_exp/1).
+
+choose_class :-
+    write('Welcome to Genshin Asik. Choose your job'), nl,
+    write('1. Knight'), nl,
+    write('2. Archer'), nl,
+    write('3. Mage'), nl,
+    read(X),nl,
+    assert_class(X).
+
+status :-
+    player_class(X),player_level(Y),player_health(Health),player_attack(Attack),player_defense(Defense),
+    player_max_health(MaxHealth),player_max_attack(MaxAttack),player_max_defense(MaxDefense),
+    current_gold(Gold),
+    write('Job: '),write(X),nl,
+    write('Level: '),write(Y),nl,
+    write('Health: '),write(Health),write('/'),write(MaxHealth),nl,
+    write('Attack: '),write(Attack),write('/'),write(MaxAttack),nl,
+    write('Defense: '),write(Defense),write('/'),write(MaxDefense),nl,
+    write('Gold: '),write(Gold),nl.
+
+exp_level_up(Level,Exp):-
+    power(1.4,Level,Exp1),
+    write(Exp1),
+    Exp is truncate(300*Exp1).
+
+assert_class(1):-
+    assertz(player_class('knight')),baseHealth(X,BaseHealth),
+    player_class(X),assertz(player_level(1)),baseHealth(X,BaseHealth),baseAttack(X,BaseAttack),baseDefense(X,BaseDefense),
+    assertz(player_health(BaseHealth)),assertz(player_attack(BaseAttack)),assertz(player_defense(BaseDefense)),
+    assertz(player_max_health(BaseHealth)),assertz(player_max_attack(BaseAttack)),assertz(player_max_defense(BaseDefense)),
+    baseWeapon(X,BaseWeapon),assertz(equipped_weapon(BaseWeapon)),assertz(current_gold(1000)),
+    write('You choose '), write(X), write(', let’s explore the world'),nl,!.
+
+assert_class(2):-
+    assertz(player_class('archer')),
+    player_class(X),assertz(player_level(1)),baseHealth(X,BaseHealth),baseAttack(X,BaseAttack),baseDefense(X,BaseDefense),
+    assertz(player_health(BaseHealth)),assertz(player_attack(BaseAttack)),assertz(player_defense(BaseDefense)),
+    assertz(player_max_health(BaseHealth)),assertz(player_max_attack(BaseAttack)),assertz(player_max_defense(BaseDefense)),
+    baseWeapon(X,BaseWeapon),assertz(equipped_weapon(BaseWeapon)),assertz(current_gold(1000)),
+    write('You choose '), write(X), write(', let’s explore the world'),nl,!.
+
+assert_class(3):-
+    assertz(player_class('mage')),
+    player_class(X),assertz(player_level(1)),baseHealth(X,BaseHealth),baseAttack(X,BaseAttack),baseDefense(X,BaseDefense),
+    assertz(player_health(BaseHealth)),assertz(player_attack(BaseAttack)),assertz(player_defense(BaseDefense)),
+    assertz(player_max_health(BaseHealth)),assertz(player_max_attack(BaseAttack)),assertz(player_max_defense(BaseDefense)),
+    baseWeapon(X,BaseWeapon),assertz(equipped_weapon(BaseWeapon)),assertz(current_gold(1000)),
+    write('You choose '), write(X), write(', let’s explore the world'),nl,!.
