7cdd5f2463eb16858d09cd9c714a78a34e47a244
Rafidika <rafidika2013@gmail.com>
2020-11-22 01:15:54 +0700

    nambah items

--- items.pl
+++ items.pl
@@ -4,6 +4,10 @@
 /* FACTS */
 
 item('health potion').
+item('panas spesial 2 mekdi').
+item('sadikin').
+item('go milk').
+item('crisbar').
 item('waster greatsword').
 item('debate club').
 item('prototype aminus').
@@ -18,8 +22,17 @@ item('pocket grimoire').
 item('emerald orb').
 item('mappa mare').
 item('skyward atlas').
+item('wooden armor').
+item('iron armor').
+item('steel armor').
+item('golden armor').
+item('diamond armor').
 
 type(consumable, 'health potion').
+type(consumable, 'panas spesial 2 mekdi').
+type(consumable, 'sadikin').
+type(consumable, 'go milk').
+ttype(consumable, 'crisbar').
 type(claymore, 'waster greatsword').
 type(claymore, 'old merc pal').
 type(claymore, 'debate club').
@@ -35,3 +48,41 @@ type(catalyst,'pocket grimoire').
 type(catalyst,'emerald orb').
 type(catalyst,'mappa mare').
 type(catalyst,'skyward atlas').
+type(armor,'wooden armor').
+type(armor,'iron armor').
+type(armor,'steel armor').
+type(armor,'golden armor').
+type(armor,'diamond armor').
+
+/* Properti buat consumable. Health nambah sebesar Arrity kedua */
+property('health potion', 75).
+property('panas spesial 2 mekdi', 150).
+property('sadikin', 225).
+property('go milk', 320).
+property('crisbar', 450).
+
+/* Properti buat claymore, bow, catalyst. Attack nambah sebesar Arrity kedua */
+property('waster greatsword', 30).
+property('old merc pal', 70).
+property('debate club', 120).
+property('prototype aminus', 180).
+property('wolf greatsword', 250).
+
+property('hunter bow', 40).
+property('seasoned hunter bow', 90).
+property('messenger', 150).
+property('favonius warbow', 230).
+property('skyward harp', 300).
+
+property('apprentice notes', 50).
+property('pocket grimoire', 110).
+property('emerald orb', 175).
+property('mappa mare', 320).
+property('skyward atlas', 410).
+
+/* Properti buat armor. Defense nambah sebesar Arrity kedua */
+property('wooden armor', 50).
+property('iron armor', 120).
+property('steel aromor', 200).
+property('golden armor', 290).
+property('diamond armor', 400).
