a6028b904c6ae1f3120fd3e6dbc7b1dcfece637a
Rafidika <rafidika2013@gmail.com>
2020-11-23 15:11:26 +0700

    nambah help dikit

--- main.pl
+++ main.pl
@@ -56,16 +56,23 @@ quit :-
 
 help :-
     game_state(in_battle),!,
-    write('Ini nanti perintah" battle').
+    write('You are currently in a battle. Here are some commands to help you get through the battle.'), nl,
+    write('Use command \'fight.\' to fight against the encountered enemy.'), nl,
+    write('Use command \'run.\' to run away from the enemy. This command might as well not work as it is and you have no choice but to fight the enemy.'),.
 
 help :-
     game_state(in_quest_dialogue),!,
-    write('Please finish your queset dialogue first before continuing').
+    write('Please finish your quest dialogue first before continuing').
 
 help :-
     game_state(shopactive),!,
-    write('Ini nanti perintah" shop').
+    write('You are currently in the shop. Here are some commands to get the stuff you needed.').
 
 help :-
     game_state(travelling),!,
-    write('Ini nanti perintah" travelling').
+    write('You are currently travlling in the outside world! Here are some commands to guide you through this fantasy map.'), nl,
+    write('Use command \'w.\' to move upward'), nl,
+    write('Use command \'a.\' to move to the left'), nl,
+    write('Use command \'s.\' to move downward'), nl,
+    write('Use command \'d.\' to move to the right'), nl,
+    write('You might encounter an enemy while you\'re travelling, so be ready for then!').
