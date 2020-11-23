606177f35bdcba8dacc7370d9e90cce88382d499
Rafidika <rafidika2013@gmail.com>
2020-11-23 15:15:40 +0700

    fix typo

--- main.pl
+++ main.pl
@@ -58,11 +58,11 @@ help :-
     game_state(in_battle),!,
     write('You are currently in a battle. Here are some commands to help you get through the battle.'), nl,
     write('Use command \'fight.\' to fight against the encountered enemy.'), nl,
-    write('Use command \'run.\' to run away from the enemy. This command might as well not work as it is and you have no choice but to fight the enemy.'),.
+    write('Use command \'run.\' to run away from the enemy. This command might as well not work as it is and you have no choice but to fight the enemy.').
 
 help :-
     game_state(in_quest_dialogue),!,
-    write('Please finish your quest dialogue first before continuing').
+    write('Please finish your quest dialogue first before continuing.').
 
 help :-
     game_state(shopactive),!,
@@ -75,4 +75,4 @@ help :-
     write('Use command \'a.\' to move to the left'), nl,
     write('Use command \'s.\' to move downward'), nl,
     write('Use command \'d.\' to move to the right'), nl,
-    write('You might encounter an enemy while you\'re travelling, so be ready for then!').
+    write('You might encounter an enemy while you\'re travelling, so be ready for them!').
